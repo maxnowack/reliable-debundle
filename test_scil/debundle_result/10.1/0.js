f1cf4d46fd97729fab481f5c505b339c5e7c6437
scil <scil.zc@gmail.com>
2020-09-26 12:38:36 +0800

    add test for inlineAndVariable

---
+++ test_scil/debundle_result/10.1/0.js
@@ -0,0 +1,5 @@
+const t = exports;
+const e = module;
+const n = require;
+'use strict';
+e.exports = require('./1');
---
+++ test_scil/debundle_result/10.1/1.js
@@ -0,0 +1,4 @@
+const t = exports;
+const e = module;
+const n = require;
+var x = require('./0'), u = require.n(x), y = 3;
